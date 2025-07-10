8d6d78b62d3022e026a676e80036f9060cfb5779
JoseZerda <jozerda@gmail.com>
2025-07-10 16:48:44 -0300

    Primer commit - Inicio

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from lec03!" << std::endl;
+    return 0;
+}
---
+++ src/util.cpp
@@ -0,0 +1,8 @@
+#include "lec03/util.hpp"
+#include <iostream>
+
+namespace lec03 {
+    void say_hello() {
+        std::cout << "Hello from util!" << std::endl;
+    }
+}
