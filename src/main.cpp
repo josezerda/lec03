61b97e18dce2330e8d1694eb81e308a9da741c7b
JoseZerda <jozerda@gmail.com>
2025-07-13 05:38:08 -0300

    Pruebas con strings

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello from lec03!" << std::endl;
+int main()
+{
+    std::cout << "Please enter your name and surname:\n";
+    std::string first_name;
+    std::string last_name;
+    std::string name;
+    double age = -1; // integer variable (-1 means "don’t know the age")
+    std::cin >> first_name >> last_name;
+    name = first_name + ' ' + last_name;
+    std::cout << "Nombre: " << first_name << std::endl;
+    std::cout << "Apellido: " << last_name << std::endl;  
+    std::cout << "Hello, " << name << std::endl;
     return 0;
 }
+
+
