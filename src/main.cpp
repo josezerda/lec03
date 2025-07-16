a0eb8f3205864dd17d85c26992230b9640b76f0f
JoseZerda <jozerda@gmail.com>
2025-07-16 10:15:47 -0300

    Años Jovianos a terrestres

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <iomanip>
 using namespace std;
 
+
 int main() {
-    cout << "Earth\tMoon\n";
-    for (int i = 1; i <= 100; ++i)
-     {
-        double moonWeight = i * 0.17;
-        cout << setw(5) << i << "\t" << fixed << setprecision(2) << moonWeight << "\n";
-        if (i % 25 == 0) cout << endl;
-    }
+    double jovianYears;
+    cout << "Enter Jovian years: ";
+    cin >> jovianYears;
+
+    double earthYears = jovianYears * 12.0;
+    cout << "Equivalent Earth years: " << fixed << setprecision(1) << earthYears << endl;
+
     return 0;
 }
