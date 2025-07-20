bf595442310eff95e469c0b2ee5261f1c0b0bb82
JoseZerda <jozerda@gmail.com>
2025-07-20 12:57:19 -0300

    nested loops and break

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,21 @@ using namespace std;
 
 int main() 
 {
-    double Principal; // original principal
-    double IntRate; // interest rate, such as 0.075
-    double PayPerYear; // number of payments per year
-    double NumYears; // number of years
-    double Payment; // the regular payment
-    double numer, denom; // temporary work variables
-    double b, e; // base and exponent for call to pow()
 
-
-    cout << "Enter principal: ";
-    cin >> Principal;
-    cout << "Enter interest rate (i.e., 0.075): ";
-    cin >> IntRate;
-    cout << "Enter number of payments per year: ";
-    cin >> PayPerYear;
-    cout << "Enter number of years: ";
-    cin >> NumYears;
-
-    numer = IntRate * Principal / PayPerYear;
-
-    e = -(PayPerYear * NumYears);
-    b = (IntRate / PayPerYear) + 1;
-    denom = 1 - pow(b, e);
-
-    Payment = numer / denom;
-    cout << "Payment is " << Payment;
+    int t, count;
+    for (t = 0; t < 10 ; t++)
+    {
+        count = 1;
+        for(;;)
+        {
+            cout << count << " ";
+            count++;
+            if(count == 16)
+            {
+                cout << endl;
+                break;
+            }
+        }
+    }
     return 0;
 }
