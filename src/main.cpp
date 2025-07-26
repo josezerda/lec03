964bba409bc174cc18943263804c18b027c9ed26
JoseZerda <jozerda@gmail.com>
2025-07-26 07:39:21 -0300

    search duplicate values

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,23 @@ using namespace std;
 
 int main() 
 {
-    int total;
-    int *ptr;
-    int val;
+    int arr[10] = {1, 4, 2, 7, 4, 2, 9, 5, 2, 7};
+    cout << "Duplicate values found: ";
+    bool found = false;
 
+    for (int i = 0; i < 10; i++) {
+        for (int j = i + 1; j < 10; j++) 
+        {
+            if (arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+                found = true;
+                break; // Avoid repeating duplicates
+            }
+        }
+    }
 
-    total = 3200;
-    ptr = &total;  //ptr gets the address of total
-
-    val = *ptr;     //val gets the value at that address
-
-    cout << "Total is; " << val << endl;
-
+    if (!found)
+        cout << "None.";
+    cout << endl;
     return 0;
-
 }
