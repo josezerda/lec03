954c3ff74af38623fd170c16fb36619ef4589a3a
JoseZerda <jozerda@gmail.com>
2025-08-15 15:25:27 -0300

    classes

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,28 @@ public:
 int main()
 {
     Vehicle minivan;
-    int range;
+    Vehicle sportscar;
+
+    int range1, range2;
 
     //Assign values to fields in minivan
     minivan.passengers = 7;
     minivan.fuelcap = 16;   // galones
     minivan.mph = 21;       //millas por galon
 
-    range = minivan.fuelcap * minivan.mph;
+    //Assign values to fields in sportscar
+    sportscar.passengers = 2;
+    sportscar.fuelcap = 14;   // galones
+    sportscar.mph = 12;       //millas por galon
+
+
+
+    range1 = minivan.fuelcap * minivan.mph;
+    range2 = sportscar.fuelcap * sportscar.mph;
 
     // Compute
-    cout << "Minivan can carry " << minivan.passengers << " a range of: " << range << " miles away" << endl;
+    cout << "Minivan can carry " << minivan.passengers << " passangers a range of: " << range1 << " miles away" << endl;
+    cout << "Sportscar can carry " << sportscar.passengers << " passangers a range of: " << range2 << " miles away" << endl;
 
     return 0;
 }
